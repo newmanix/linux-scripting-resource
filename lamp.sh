8cbed0ce5ebec289b8ac96baf1951d19a6c8a096
Ariton84 <vanver@gmail.com>
2016-11-19 20:32:57 -0800

    Updated Script
    updated script with current directions to install LAMP stack

--- lamp.sh
+++ lamp.sh
@@ -2,40 +2,34 @@
 
 #Instructions to use this script 
 #
-#chmod +x LAMP.sh
+#chmod +x lamp.sh
 #
-#sudo ./LAMP.sh
+#sudo ./lamp.sh
 
+echo "###################################################################################"
+echo "Installation will now begin"
 echo "###################################################################################"
 
 #Update the repositories
 
 sudo apt-get update
 
-#Apache, Php, MySQL and required packages installation
+#Apache installation
 
-sudo apt-get -y install apache2 php5 libapache2-mod-php5 php5-mcrypt php5-curl php5-mysql php5-gd php5-cli php5-dev mysql-client
-php5enmod mcrypt
+sudo apt-get -y install apache2 
 
-#The following commands set the MySQL root password to MYPASSWORD123 when you install the mysql-server package.
+#The following commands install and set the MySQL root password to MYPASSWORD123 when you install the mysql-server package.
 
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password MYPASSWORD123'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password MYPASSWORD123'
 
-sudo apt-get -y install mysql-server
+sudo apt-get -y install mysql-server mysql-client
 
-#Restart all the installed services to verify that everything is installed properly
+#Php installation
 
-echo -e "\n"
+sudo apt-get -y install php7.0-mysql php7.0-curl php7.0-json php7.0-cgi php7.0 libapache2-mod-php7 
 
-service apache2 restart && service mysql restart > /dev/null
+#Reset Apache and verify everything works
 
-echo -e "\n"
+sudo systemctl restart apache2
 
-if [ $? -ne 0 ]; then
-   echo "Please Check the Install Services, There is some $(tput bold)$(tput setaf 1)Problem$(tput sgr0)
-else
-   echo "Installed Services run $(tput bold)$(tput setaf 2)Sucessfully$(tput sgr0)"
-fi
-
-echo -e "\n"
